f1a497973cbb94805a5d910988a366681c170919
shivampahwa9 <shivampahwa.9@gmail.com>
2018-05-26 21:39:43 +0530

    extract more tweets

--- twitter_analysis.py
+++ twitter_analysis.py
@@ -172,6 +172,156 @@ def tweeting():
     api=tweepy.API(auth)
     api.update_status(text)
 
+def for_narendra_modi():
+    print("Retreving tweets")
+    api=tweepy.API(auth)
+    Tweets = api.user_timeline(screen_name= "@narendramodi", count=200)  ###Search for tweets by the given screen name
+    for data in Tweets:
+        tweet = TextBlob(data.text)
+        print(tweet)
+        print(tweet.sentiment)  ### For analysis of sentiments
+        print("Done at :" + str(data.created_at))  ### Print the time for the creation of tweet
+        print("Username :" + data.user.name)  ### Print the user name of the twitter user
+        print("---------------------------------------------")
+    query = []  ### Stores the keywords to be searched for
+    tweets = []  ### to store the tweets
+    tweets2 = []  ### to store modified tweets
+    shows = True
+    while shows:  ### to store the no of keywords till we want
+        qw = input("Enter  the keywords ro be searched for :")
+        query.append(qw)
+        ask = input("Do you want to add more keywords?(Y/N)")
+        ask = ask.upper()
+        if ask == "Y":
+            pass
+        elif ask == "N":
+            shows = False
+        else:
+            print("invalid input  \n"
+                  "Taking default input as 'No' ")
+            shows = False
+
+    for data in Tweets:
+        tweets.append(data)
+    oldest = tweets[-1].id  ### stores the id of the last tweet retrieved
+    oldest_at = tweets[-1].created_at  ### Stores the time and date of the last tweet retrieved
+    qu = True
+    while qu:  ###to retrieve more older tweets
+        qu2 = input("Do you want  more  tweets older than " + str(oldest_at) + "(Y/N)  :")
+        qu2 = qu2.upper()
+        if qu2 == "Y":
+            result = api.user_timeline(screen_name="@narendramodi", count=200,
+                                        max_id=oldest)  ###retrieve the tweet older then the given tweet id
+            for data in result:
+                tweets.append(data)  ###  appended the new tweets retrived
+            oldest = tweets[-1].id
+            oldest_at = tweets[-1].created_at
+        elif qu2 == "N":
+            qu = False
+        else:
+            print("invalid input  \n"
+                  "Taking default input as 'No' ")
+            qu = False
+    count = -1
+    for tweet1 in tweets:
+        count = count + 1  ###starts the list from beginning
+        tweet = tweet1.text
+        tweet = re.sub(r"http\S+", "", tweet)  ##### removes the URL from the text of tweet
+        tweets[count].text = tweet
+    for keyword in query:
+        for data in Tweets:
+            tex = TextBlob(data.text)
+            qw = tex.find(keyword)
+            if qw != -1:
+                tweets2.append(data)  ### Appends the modified tweets
+            else:
+                pass
+    if len(tweets2) > 0:
+        for data in tweets:
+            tweet = TextBlob(data.text)
+            print(tweet)
+            print(tweet.sentiment)  ### For analysis of sentiments
+            print("Done at :" + str(data.created_at))  ### Print the time for the creation of tweet
+            print("Username :" + data.user.name)  ### Print the user name of the twitter user
+            print("---------------------------------------------")
+    else:
+        print("No tweet found with the related keywords")
+
+def for_donald_trump():
+    print("Retreving tweets")
+    api =tweepy.API(auth)
+    Tweets = api.user_timeline(screen_name="@realdonaldtrump", count=200)  ###Search for tweets by the given screen name
+    for data in Tweets:
+        tweet = TextBlob(data.text)
+        print(tweet)
+        print(tweet.sentiment)
+        print("Done at :" + str(data.created_at))
+        print("Username :" + data.user.name)
+        print("---------------------------------------------")
+    query = []  ### Stores the keywords to be searched for
+    tweets = []  ### to store the tweets
+    tweets2 = []  ### to store modified tweets
+    shows = True
+    while shows:  ### to store the no of keywords till we want
+        qw = input("Enter  the keywords ro be searched for :")
+        query.append(qw)
+        ask = input("Do you want to add more keywords?(Y/N)")
+        ask = ask.upper()
+        if ask == "Y":
+            pass
+        elif ask == "N":
+            shows = False
+        else:
+            print("invalid input  \n"
+                  "Taking default input as 'No' ")
+            shows = False
+
+    for data in Tweets:
+        tweets.append(data)
+    oldest = tweets[-1].id  ### stores the id of the last tweet retrieved
+    oldest_at = tweets[-1].created_at  ### Stores the time and date of the last tweet retrieved
+    qu = True
+    while qu:  ###to retrieve more older tweets
+        qu2 = input("Do you want  more  tweets older than " + str(oldest_at) + "(Y/N)  :")
+        qu2 = qu2.upper()
+        if qu2 == "Y":
+            result = api.user_timeline(screen_name="@realdonaldtrump", count=200,
+                                        max_id=oldest)  ###retrieve the tweet older then the given tweet id
+            for data in result:
+                tweets.append(data)  ###  appended the new tweets retrived
+            oldest = tweets[-1].id
+            oldest_at = tweets[-1].created_at
+        elif qu2 == "N":
+            qu = False
+        else:
+            print("invalid input  \n"
+                  "Taking default input as 'No' ")
+            qu = False
+    count = -1
+    for tweet1 in tweets:
+        count = count + 1  ###starts the list from beginning
+        tweet = tweet1.text
+        tweet = re.sub(r"http\S+", "", tweet)  ##### removes the URL from the text of tweet
+        tweets[count].text = tweet
+    for keyword in query:
+        for data in Tweets:
+            tex = TextBlob(data.text)
+            qw = tex.find(keyword)
+            if qw != -1:
+                tweets2.append(data)
+            else:
+                pass
+    if len(tweets2) > 0:
+        for data in tweets:
+            tweet = TextBlob(data.text)
+            print(tweet)
+            print(tweet.sentiment)
+            print("Done at :" + str(data.created_at))
+            print("Username :" + data.user.name)
+            print("---------------------------------------------")
+    else:
+        print("No tweet found with the related keywords")
+
 
 
 
@@ -184,7 +334,9 @@ def menu():
                             "5.Search for a specific keyword\n"
                             "6.Extract info about the tweets\n"
                             "7.Remove stopwords from tweets\n"
-                            "8.Exit\n"
+                            "8.Extract Tweets for Narender Modi \n"
+                            "9.Extract Tweets for Donald Trump\n"
+                            "10.Exit\n"
                             ))
     if user_choice == 1:
         search_tweet()
@@ -213,10 +365,14 @@ def menu():
     elif user_choice == 7:
         remove_stopwords()
 
+    elif user_choice == 8:
+        for_narendra_modi()
+
+    elif user_choice ==9:
+        for_donald_trump()
+
     else:
         pass
-
-
 menu()
 
 
